42fe36f71e158830316cf6b98dc31c8799f2da9f
Maxwell <codingxam@gmail.com>
2023-07-07 23:46:35 -0400

    creating initial routes

---
+++ src/routes/completedTask.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+
+router.get('/');
+router.get('/:id');
+router.post('/');
+router.put('/:id')
+router.delete('/:id')
+
+
+module.exports = router
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const router = Router();
+
+
+router.get('/');
+router.get('/:id');
+router.post('/');
+router.put('/:id')
+router.delete('/:id')
+
+
+
+
+
+module.exports = router;
