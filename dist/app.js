e3113e2cc5b2a367f0946d3e443528d3a7755e30
Maxwell <codingxam@gmail.com>
2023-07-23 13:19:20 -0400

    agregado dist

--- dist/app.js
+++ dist/app.js
@@ -6,10 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const PORT = '3001';
-const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.listen(PORT || process.env.PORT, () => {
-    console.log('server connected');
-});
+const users_routes_1 = __importDefault(require("./routes/users.routes"));
+const tasks_routes_1 = __importDefault(require("./routes/tasks.routes"));
+(() => {
+    const PORT = '3001';
+    const app = (0, express_1.default)();
+    app.use((0, cors_1.default)());
+    app.use(express_1.default.json());
+    //Router
+    app.use('/api/user', users_routes_1.default);
+    app.use('/api/task', tasks_routes_1.default);
+    app.use('/', (req, res) => {
+        res.json({ status: 'Api is running' });
+    });
+    app.listen(PORT || process.env.PORT, () => {
+        console.log('server connected');
+    });
+})();
